f69dbb5b34279604cb67ef53e5c2fba37d4ccac7
Alba <albavillalonlucas@gmail.com>
2017-05-23 18:42:45 +0200

    Bean user & DAO

---
+++ src/controllers/DAO.java
@@ -0,0 +1,62 @@
+package controllers;
+
+import java.sql.*;
+
+
+public class DAO {
+	private Connection connection;
+	private Statement statement;
+
+	public DAO() throws Exception {
+	String user = "mysql";
+	String password="prac";
+	
+Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+	
+	connection=DriverManager.getConnection("jdbc:mysql://localhost/ts1?user="+user+"&password="+password);
+
+	statement=connection.createStatement();
+	String sqlCreate = "CREATE DATABASE IF NOT EXISTS practica2";
+	statement = connection.createStatement();
+	statement.execute(sqlCreate);
+
+	sqlCreate = "USE practica2";
+	statement = connection.createStatement();
+	statement.execute(sqlCreate);
+	
+	sqlCreate = "CREATE TABLE IF NOT EXISTS users"
+            + "  (user CHAR(24),"
+            + "   name CHAR(50),"
+            + "   middleName CHAR(50),"
+            + "   lastName CHAR(50),"
+            + "   mail CHAR(50),"
+            + "   password CHAR(50),"
+            + "   birthdate CHAR(50),"
+            + "   profilePicture CHAR(100),"
+            + "   id INTEGER NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))";
+
+	
+	statement = connection.createStatement();
+	statement.execute(sqlCreate);
+
+	connection = DriverManager.getConnection("jdbc:mysql://localhost/practica2?user=" + user + "&password=" + password);
+
+	
+
+	
+	}
+	//execute queries
+	public ResultSet executeSQL(String query) throws SQLException{
+		return statement.executeQuery(query);
+	}
+	public int executeInsert(String query) throws SQLException {
+		return statement.executeUpdate(query);
+	}
+
+	//TODO: code for updates for Assignments 2, 3 and 4.
+	public void disconnectBD() throws SQLException{
+		statement.close();
+		connection.close();
+	}
+}
--- src/models/BeanUser.java
+++ src/models/BeanUser.java
@@ -1,12 +1,24 @@
 package models;
+import java.sql.ResultSet;
 
+import controllers.DAO;
 public class BeanUser implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private String user = "";
 	private String mail = "";
-	private int[] error = {1,0}; 
+	private String name = "";
+	private String middleName = "";
+	private String lastName = "";
+	private String password = "";
+	private String confirmationPassword = "";
+	private String birthdate = "";
+	private String profilePicture = "";
+
+	
+	/*  Control which parameters have been correctly filled */
+	private int[] error = {0,0}; 
 	
 	/* Getters */
 	public String getUser(){
@@ -17,23 +29,101 @@ public class BeanUser implements java.io.Serializable {
 		return mail;
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getConfirmationPassword() {
+		return confirmationPassword;
+	}
+	
+	public String getBirthdate() {
+		return birthdate;
+	}
+	
 	public int[] getError() {
 		return error;
 	}
 	
 	/*Setters*/
 	public void setUser(String user){
-		this.user = user;
+		try {
+			DAO dao = new DAO();
+			ResultSet f = dao.executeSQL("SELECT COUNT(*) FROM users WHERE user='" + user + "';");
+			f.next(); int i = Integer.parseInt(f.getString(1));
+			if(i!=0)setError(0,1);
+			
+			else this.user = user;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void setMail(String mail){
-		this.mail = mail;
+		try {
+			DAO dao = new DAO();
+			ResultSet f = dao.executeSQL("SELECT COUNT(*) FROM users WHERE mail='" + mail + "';");
+			f.next(); int i = Integer.parseInt(f.getString(1));
+			if(i!=0)setError(1,1); 
+			else this.mail = mail;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setConfirmationPassword(String confirmationPassword) {
+		this.confirmationPassword = confirmationPassword;
+	}
+	
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}	
+
+	public void setError(int pos, int error) {
+		//En la primera posicin va el error del user, 
+		// en la segunda el error del mail
+		this.error[pos] = error;
 	}
 	
 	/* Logic Functions */
 	public boolean isComplete() {
 	    return(hasValue(getUser()) &&
-	           hasValue(getMail()) );
+	           hasValue(getMail()) &&
+	           hasValue(getName()) &&
+	           hasValue(getLastName()) &&
+	           hasValue(getPassword()) &&
+	           hasValue(getBirthdate()));
 	}
 	
 	private boolean hasValue(String val) {
